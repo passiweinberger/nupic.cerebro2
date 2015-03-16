46e0078d439ac8b397d0efed4f4544ff2240e151
Pascal Weinberger <passiweinberger@gmail.com>
2015-03-16 20:04:16 +0100

    Update run_server.py

--- run_server.py
+++ run_server.py
@@ -7,7 +7,7 @@ import subprocess
 """
 ## Usage
 Make sure the 'nupic.cerebro2.server directory' is extracted in here and all requirements are installed. 
-(Furter information : see the READMEs)
+(Further information : see the READMEs)
 
 First, patch your model:
 
@@ -24,7 +24,6 @@ You can also patch an SP or a TP directly:
 
 Then, after the model / SP / TP has through a number of iterations:
 
-
 Then, run:
 
     ./run_server <PortNumber>
@@ -33,7 +32,6 @@ Then, run:
 
 To enable taking screenshots with the `p` keyboard shortcut, open `js/classes/abstract_visualization.js` and uncomment the line under the note about screenshots.
 
-@author: passiweinberger
 """
 
 DESCRIPTION = ("This is to set up a Cerebro2 server and open it in your browser.\n"
@@ -52,34 +50,34 @@ DESCRIPTION = ("This is to set up a Cerebro2 server and open it in your browser.
     		"Patcher().patchSP(sp)\n"
     		"Patcher().patchTP(tp)\n\n"
     		
-		"This scripc also assumes that you have the 'nupic.cerebro2.server' folder inside this directory!\n"
+		"This script also assumes that you have the 'nupic.cerebro2.server' folder inside this directory!\n"
 		"For further information read the READMEs in these two folders!\n\n"
-		"If last lines for Exit Cmd uncommented:\n"
-		"Press C-c to kill all the processes. \n"
+		"If last lines for Exit command uncommented:\n"
+		"Press Ctrl-C to kill all the processes. \n"
 		"(CAUTION: executes 'killall python' !!)\n")
 
 		
-def server_setup(StartDir):
-    ServerDir = os.path.join(StartDir, 'nupic.cerebro2.server')
-    if not os.path.isdir(ServerDir):
+def serverSetup(startDir):
+    serverDir = os.path.join(startDir, 'nupic.cerebro2.server')
+    if not os.path.isdir(serverDir):
       print "Could not find the nupic.cerebro2.server directory! Please copy it into this Directory!\nTerminated."
       return 1
     else:
-      os.chdir(ServerDir)
+      os.chdir(serverDir)
       print "Server is being set up...\n"
       subprocess.Popen(["python", "server.py"])
 
 
-def server_call(StartDir, port):
-    StaticDir = os.path.join(StartDir, 'static')
-    os.chdir(StaticDir)
+def serverCall(startDir, port):
+    staticDir = os.path.join(startDir, 'static')
+    os.chdir(staticDir)
     print "Calling the server at port number %s" % port 
     command = "python -m SimpleHTTPServer " + str(port)    
     subprocess.Popen(["python", "-m", "SimpleHTTPServer", port])    
     print "Server is up and running at port %s!\n" % port
 
 
-def open_browser(port):
+def openBrowser(port):
     print "Opening port %s in your default Browser..." % port 
     url = "http://localhost:%d" % int(port)
     os.spawnl(os.P_NOWAIT, webbrowser.open_new_tab(url))
@@ -91,17 +89,17 @@ if __name__ == "__main__":
     print "Please supply a port to be run on!\nTerminated."
   else:
     print DESCRIPTION
-    StartDir = os.getcwd()
+    startDir = os.getcwd()
     port = sys.argv[1]
-    #Setting up the server in nupic.cerebro2.server module:
-    server_setup(StartDir)
-    #Calling the server for the provided port:
-    server_call(StartDir, port) 
-    #Opening the browser at the port:
-    open_browser(port)
+    # Setting up the server in nupic.cerebro2.server module:
+    serverSetup(startDir)
+    # Calling the server for the provided port:
+    serverCall(startDir, port) 
+    # Opening the browser at the port:
+    openBrowser(port)
 
-    #Exit command:
+    # Exit command:
     #if KeyboardInterrupt:
-    #	os.system("gnome-terminal -e 'killall python'")
+    #	subprocess.Popen(["killall", "python"])
 	
 
