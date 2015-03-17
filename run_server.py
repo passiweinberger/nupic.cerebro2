4b68670bf407c00d068d96d2e71db9615f803ab5
Pascal Weinberger <passiweinberger@gmail.com>
2015-03-17 17:45:29 +0100

    Update Docstring; remove doubles

--- run_server.py
+++ run_server.py
@@ -4,35 +4,6 @@ import os
 import sys
 import webbrowser
 import subprocess
-"""
-## Usage
-Make sure the 'nupic.cerebro2.server directory' is extracted in here and all requirements are installed. 
-(Further information : see the READMEs)
-
-First, patch your model:
-
-    // Assuming `model` is a HTM model you already have
-    from cerebro2.patcher import Patcher
-    Patcher().patchCLAModel(model)
-
-You can also patch an SP or a TP directly:
-
-    // Assuming `sp` and `tp` are already defined
-    from cerebro2.patcher import Patcher
-    Patcher().patchSP(sp)
-    Patcher().patchTP(tp)
-
-Then, after the model / SP / TP has through a number of iterations:
-
-Then, run:
-
-    ./run_server <PortNumber>
-
-### Screenshots
-
-To enable taking screenshots with the `p` keyboard shortcut, open `js/classes/abstract_visualization.js` and uncomment the line under the note about screenshots.
-
-"""
 
 DESCRIPTION = ("This is to set up a Cerebro2 server and open it in your browser.\n"
 		"It is assumed, that you already patched your model or the SP & TP parameters.\n"
@@ -49,6 +20,11 @@ DESCRIPTION = ("This is to set up a Cerebro2 server and open it in your browser.
     		"from cerebro2.patcher import Patcher\n"
     		"Patcher().patchSP(sp)\n"
     		"Patcher().patchTP(tp)\n\n"
+    		
+    		"Then, after the model / SP / TP has through a number of iterations:\n"
+    		"./run_server <PortNumber>\n\n"
+    		"To enable taking screenshots with the `p` keyboard shortcut, open `js/classes/abstract_visualization.js` \n"  
+    		"and uncomment the line under the note about screenshots.\n"
     		
 		"This script also assumes that you have the 'nupic.cerebro2.server' folder inside this directory!\n"
 		"For further information read the READMEs in these two folders!\n\n"
